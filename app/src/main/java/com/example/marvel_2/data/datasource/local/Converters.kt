d98ce8bcc5c2fe65f532a8ebfd823fb679ac2814
ChristalCamillo <christal.b.camillo@gmail.com>
2022-11-01 15:11:34 -0300

    criação das divisões mvvm

---
+++ app/src/main/java/com/example/marvel_2/data/datasource/local/Converters.kt
@@ -0,0 +1,16 @@
+package com.example.marvel_2.data.datasource.local
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+
+class Converters {
+    @TypeConverter
+    fun converterFromPersonagem(value: String): Personagem? {
+        return Gson().fromJson(value, Personagem::class.java)
+    }
+
+    @TypeConverter
+    fun converterToJson(char: Personagem): String {
+        return Gson().toJson(char)
+    }
+}
